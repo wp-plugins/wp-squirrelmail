62cd03d620e395fbb0e86d54e75ec9d14b7fc5cd
edgarr41 <edgarr41@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-01 08:03:05 +0000

    fixed uninstall.php
    git-svn-id: https://plugins.svn.wordpress.org/wp-squirrelmail/trunk@1150839 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- uninstall.php
+++ uninstall.php
@@ -4,6 +4,12 @@ if ( !defined( 'WP_UNINSTALL_PLUGIN' ) ) {
     exit();
 }
 
-delete_option( 'wpsquirrelmail_user_settings' );
+$meta_type  = 'user';
+$user_id    = 0; // This will be ignored, since we are deleting for all users.
+$meta_key   = 'wpsquirrelmail_user_settings';
+$meta_value = ''; // Also ignored. The meta will be deleted regardless of value.
+$delete_all = true;
+
+delete_metadata( $meta_type, $user_id, $meta_key, $meta_value, $delete_all );
 delete_option( 'wpsquirrelmail_settings_action' );
 delete_option( 'wpsquirrelmail_settings_capability' );
